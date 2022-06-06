d6fae609165557f1a8e46aa8a3dff3f08ef37c55
Arman Karimov <krmv.kz@gmail.com>
2022-06-06 20:15:38 +0600

    Initial Commit

---
+++ LiveScore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveScore
+//
+//  Created by Arman Karimov on 06.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
